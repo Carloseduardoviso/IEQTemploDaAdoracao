7b1a743aa4c01fd8bfc903ebab50cc1a643aecb5
Carlos Eduardo <carloseduardo384.45@gmail.com>
2025-05-13 13:43:55 -0400

    criando menu

---
+++ Controllers/SobreQuadrangularController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IEQTemploDaAdoracao.Controllers
+{
+    public class SobreQuadrangularController : Controller
+    {
+        public IActionResult HistoriaDaQuadrangular()
+        {
+            return View();
+        }
+      
+        public IActionResult NoQueCremos()
+        {
+            return View();
+        }
+
+        public IActionResult DoutrinaQuadrangular()
+        {
+            return View();
+        }
+
+        public IActionResult NossaMissão()
+        {
+            return View();
+        }
+
+        public IActionResult OrganizaçãoQuadrangular()
+        {
+            return View();
+        }
+    }
+}
